b0ff694a92d7d137cecddb6d796ab4c5d00588b4
Ryan Drost <ryan.s.drost@gmail.com>
2013-11-17 09:10:22 -0600

    update configuration

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 TWITTER_CONFIG = YAML.load_file("#{::Rails.root}/config/twitter.yml")[::Rails.env]
 
 # Initialize the rails application
-TrueGM::Application.initialize!
+EvokeTruegmRails::Application.initialize!
