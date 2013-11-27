a638b256607daabaa2ea8cea10532c5d5375fcba
Ryan Drost <ryan.s.drost@gmail.com>
2013-11-27 15:10:38 +0000

    add params to devise strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,18 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
 
+  before_filter :configure_devise_params, if: :devise_controller?
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :name)
+    end 
+    
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :name, :current_password)
+    end 
+  end
+  
   def after_sign_in_path_for(resource)
     leagues_path
   end
