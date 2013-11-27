07aa630b5f546f1a983bd676df6b9d58df11207c
Ryan Drost <ryan.s.drost@gmail.com>
2013-11-27 15:11:33 +0000

    Add name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130410233039) do
+ActiveRecord::Schema.define(version: 20131127143914) do
 
   create_table "draft_picks", force: true do |t|
     t.integer  "team_id"
@@ -103,6 +103,25 @@ ActiveRecord::Schema.define(version: 20130410233039) do
     t.datetime "updated_at"
   end
 
+  create_table "owners", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+  end
+
+  add_index "owners", ["email"], name: "index_owners_on_email", unique: true, using: :btree
+  add_index "owners", ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true, using: :btree
+
   create_table "power_rankings", force: true do |t|
     t.integer  "team_id"
     t.text     "description"
@@ -178,6 +197,7 @@ ActiveRecord::Schema.define(version: 20130410233039) do
     t.string   "provider"
     t.string   "uid"
     t.boolean  "is_admin"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
