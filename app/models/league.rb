d0aa68147e3b2638f0a6f48bb27328582b4124ea
Ryan Drost <ryan.s.drost@gmail.com>
2014-02-22 06:08:50 +0000

    added get_team_name_for_player method

--- app/models/league.rb
+++ app/models/league.rb
@@ -5,4 +5,14 @@ class League < ActiveRecord::Base
 
   validates_presence_of :tagline
   validates_length_of :name, :minimum => 5, :maximum => 20, :allow_blank => false
+
+  def get_team_name_for_player nfl_player
+    team = Team.joins(:nfl_players)
+               .where(:teams => {:league_id => id}, :nfl_players => {:id => nfl_player.id})
+               .first
+
+    return "FA" if team.nil?
+    team.name
+  end
+
 end
