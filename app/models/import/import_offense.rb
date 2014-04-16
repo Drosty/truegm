d382d6600ca7521db039362254174314d7379e36
Ryan Drost <rdrost@nerdery.com>
2014-04-15 19:11:04 -0500

    set stats on import

--- app/models/import/import_offense.rb
+++ app/models/import/import_offense.rb
@@ -1,9 +1,11 @@
 module Import
   class ImportOffense
 
-    attr_accessor :Rk, :Player, :Team, :Pos, :PassYds, :TD,
-                  :Int, :RushYds, :Rec, :Yds, :FumLost,
-                  :FantasyPoints, :year, :week
+    #TD is just there for the initializer part, not used
+    #Yds is reception yards
+    attr_accessor :Rk, :Player, :Team, :Pos, :PassYds, :PassTDs,
+                  :Int, :RushYds, :RushTds, :Rec, :Yds, :RecTds, :FumLost,
+                  :FantasyPoints, :year, :week, :TD
 
     def initialize(attributes = {})
       attributes.each do |name, value|
@@ -12,8 +14,13 @@ module Import
     end
 
     def process_player_stats
+      return unless NflPlayer.available_positions_for_filter.include?(self.Pos.downcase)
+
       player = create_or_update_and_return_player_model
       player.save
+
+      stat = create_or_update_and_return_stats_model player.id
+      stat.save
     end
 
   private
@@ -23,9 +30,23 @@ module Import
       player.first_name = first_name
       player.last_name = last_name
       player.full_name = self.Player
-      player.position = self.Pos
+      player.position = self.Pos.downcase
       player.nfl_team = get_nfl_team
       player
+    end
+
+    def create_or_update_and_return_stats_model player_id
+      stat = Stat.find_or_create_by(week: self.week, year: self.year, nfl_player_id: player_id)
+      stat.passing_yards = self.PassYds
+      stat.passing_touchdowns = self.PassTDs
+      stat.interceptions = self.Int
+      stat.rushing_yards = self.RushYds
+      stat.rushing_touchdowns = self.RushTds
+      stat.receptions = self.Rec
+      stat.receiving_yards = self.Yds
+      stat.receiving_touchdowns = self.RecTds
+      stat.fumbles_lost = self.FumLost
+      stat
     end
 
     def nfl_player_import_id
