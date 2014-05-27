c014af40f7b374090b14a35c45f0497f57ba9455
Ryan Drost <rdrost@nerdery.com>
2014-05-26 19:22:34 -0500

    add player helper for display team name
    takes defensive players into account

--- app/helpers/nfl_players_helper.rb
+++ app/helpers/nfl_players_helper.rb
@@ -1,8 +1,20 @@
 module NflPlayersHelper
+
   def sortable_link(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
+
+  def display_team_name player
+    display = ""
+    if player.position != "def" && player.nfl_team != nil
+      display = "#{player.nfl_team.location} #{player.nfl_team.mascot}"
+    elsif player.position != "def"
+      display = "Free Agent"
+    end
+    display
+  end
+
 end
