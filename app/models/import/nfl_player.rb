925a0744883e1f6d5259b06399eb23c6ed253a18
Ryan Drost <r.drost@nerdery.com>
2014-05-30 13:52:48 -0500

    add nfl_player import model

---
+++ app/models/import/nfl_player.rb
@@ -0,0 +1,7 @@
+module Import
+  class NflPlayer < BaseImport
+    attr_accessor :playerId, :active, :jersey, :lname, :fname, :displayName,
+                  :team, :position, :height, :weight, :college, :dob
+
+  end
+end
