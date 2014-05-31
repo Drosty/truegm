8b42580c4134283dcac4edd5cae3843a54a02b07
Ryan Drost <rdrost@nerdery.com>
2014-05-31 10:14:37 -0500

    add api_key as a variable and set in initializer

--- app/services/fantasy_football_nerd_party.rb
+++ app/services/fantasy_football_nerd_party.rb
@@ -3,8 +3,12 @@ class FantasyFootballNerdParty
 
   base_uri "www.fantasyfootballnerd.com/service"
 
+  def initialize(api_key = "test")
+    @key = api_key
+  end
+
   def nfl_teams
-    response = self.class.get("/nfl-teams/json/test/")
+    response = self.class.get("/nfl-teams/json/#{@key}/")
     response.parsed_response["NFLTeams"].map do |team|
       Import::NflTeam.new(team)
     end
@@ -14,11 +18,11 @@ class FantasyFootballNerdParty
     rtn_response = nil
     if position.blank?
       # get all players, all positions
-      response = self.class.get("/players/json/test/")
+      response = self.class.get("/players/json/#{@key}/")
       rtn_response = response.parsed_response
     else
       # get position
-      response = self.class.get("/players/json/test/#{position.upcase}/")
+      response = self.class.get("/players/json/#{@key}/#{position.upcase}/")
       rtn_response = response.parsed_response
     end
 
@@ -28,24 +32,24 @@ class FantasyFootballNerdParty
   end
 
   def stats player_id
-    response = self.class.get("/player/json/test/#{player_id}/")
+    response = self.class.get("/player/json/#{@key}/#{player_id}/")
     response.parsed_response["Stats"]
   end
 
   def player_videos player_id
-    response = self.class.get("/player/json/test/#{player_id}/")
+    response = self.class.get("/player/json/#{@key}/#{player_id}/")
     response.parsed_response["Videos"]
   end
 
   def nfl_schedule
-    response = self.class.get("/schedule/json/test/")
+    response = self.class.get("/schedule/json/#{@key}/")
     response.parsed_response["Schedule"].map do |schedule|
       Import::Schedule.new(schedule)
     end
   end
 
   def current_week
-    response = self.class.get("/schedule/json/test/")
+    response = self.class.get("/schedule/json/#{@key}/")
     response.parsed_response["currentWeek"].to_i
   end
 
