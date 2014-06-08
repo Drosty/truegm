6303443959fa2a99a3a33bcd96d57f37bbfa826a
Ryan Drost <rdrost@nerdery.com>
2014-06-08 16:30:57 -0500

    match ups method that returns both away and home matchups

--- app/models/nfl_team.rb
+++ app/models/nfl_team.rb
@@ -5,15 +5,16 @@ class NflTeam < ActiveRecord::Base
   has_many :home_matchups, :foreign_key => 'home_team_id', :class_name => "NflMatchup"
   has_many :away_matchups, :foreign_key => 'away_team_id', :class_name => "NflMatchup"
 
+  def matchups
+    NflMatchup.where("home_team_id = ? OR away_team_id = ?", self.id, self.id)
+  end
+
   # this will return the NFL Team that this NFL Team is player and then
   # if it 'vs' or 'at' as a string
   def week_nfl_matchup week
     year = ENV["current_year"]
 
-    matchup = home_matchups.where(year: year, week: week).first
-    return matchup unless matchup.nil?
-
-    matchup = away_matchups.where(year: year, week: week).first
+    matchup = matchups.where(year: year, week: week).first
     return matchup
   end
 
