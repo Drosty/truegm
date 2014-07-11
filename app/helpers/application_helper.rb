3f2755b23d6a3eccb018246539ee0730a7a87b3c
Ryan Drost <r.drost@nerdery.com>
2014-07-10 19:11:18 -0500

    add format_as_money helper and update displays

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
     return "#{player.position.upcase} - #{team_code}"
   end
 
+  def format_as_money input_string
+    number_to_currency input_string, precision: 0
+  end
+
 end
