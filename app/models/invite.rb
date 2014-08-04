82de8bd0a817751e37f508e922077b9d3f24c7c3
Ryan Drost <rdrost@nerdery.com>
2014-08-03 22:01:54 -0500

    provide email and redirect to edit path with message

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,5 +1,14 @@
 class Invite < ActiveRecord::Base
+  attr_accessible :team_id, :sender_id, :recipient_id, :email
+
   belongs_to :team
   belongs_to :sender, :class_name => 'User'
   belongs_to :recipient, :class_name => 'User'
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = Digest::SHA1.hexdigest([self.team_id, Time.now, rand].join)
+  end
+
 end
