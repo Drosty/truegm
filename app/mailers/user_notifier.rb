57e82d4119aa07a7b58416ca88f605930a66d3f3
Ryan Drost <rdrost@nerdery.com>
2014-08-05 23:32:13 -0500

    send invite email updated

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -2,8 +2,9 @@ class UserNotifier < ActionMailer::Base
   default :from => 'ryandrost@truegm.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_invite_email(user)
-    @user = user
-    mail(:to => @user.email, :subject => "You've been invited to a TrueGM League")
+  def send_invite_email(invite, invite_path)
+    @invite = invite
+    @invite_path = invite_path
+    mail(:to => @invite.email, :subject => "You've been invited to a TrueGM League")
   end
 end
