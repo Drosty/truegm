d37ac9a6adb4c95b8a956b6be75babb9bb676347
Ryan Drost <rdrost@nerdery.com>
2014-08-23 17:43:42 -0500

    removing tests - pages no longer exist
    Going to have to change these tests to change so that they are checking
    JSON results etc.

--- spec/controllers/league_controller_spec.rb
+++ spec/controllers/league_controller_spec.rb
@@ -20,11 +20,6 @@ describe LeaguesController do
       user = create(:user)
       login(user)
     end
-
-    it "can get to league index page" do
-      visit leagues_path
-      page.should have_content(I18n.t('leagues.header'))
-    end
   end
-  
+
 end
