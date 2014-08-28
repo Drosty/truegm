4131778d27ee54f7ace65967fc542d25c7cbc2a8
Ryan Drost <rdrost@nerdery.com>
2014-08-27 21:27:19 -0500

    show My Team link in navigation bar #48

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,10 @@ class User < ActiveRecord::Base
   has_many :invitations, :class_name => "Invite", :foreign_key => 'recipient_id'
   has_many :sent_invites, :class_name => "Invite", :foreign_key => 'sender_id'
 
+  def team_in_league(league_id)
+    teams.where(league_id: league_id).first
+  end
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
