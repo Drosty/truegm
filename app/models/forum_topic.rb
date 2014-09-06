94b6ccbf7ec4c3a3f14aa0fe120bb47f125c0467
Ryan Drost <rdrost@nerdery.com>
2014-09-05 23:38:56 -0500

    manage forum post and topics

--- app/models/forum_topic.rb
+++ app/models/forum_topic.rb
@@ -1,6 +1,6 @@
 class ForumTopic < ActiveRecord::Base
   default_scope { order('updated_at desc') }
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :user_id, :league_id
 
   has_many :forum_posts
   belongs_to :league
