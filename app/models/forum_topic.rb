c24ba34397cbb6ed1f1845be5fbd471d462d2be9
Ryan Drost <rdrost@nerdery.com>
2014-09-05 23:42:26 -0500

    delete dependent on forums

--- app/models/forum_topic.rb
+++ app/models/forum_topic.rb
@@ -2,7 +2,7 @@ class ForumTopic < ActiveRecord::Base
   default_scope { order('updated_at desc') }
   attr_accessible :description, :name, :user_id, :league_id
 
-  has_many :forum_posts
+  has_many :forum_posts, :dependent => :destroy
   belongs_to :league
   belongs_to :user
 
