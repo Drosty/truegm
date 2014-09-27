5c8b8f9a198865d5ebb977e0ae09163d43d338d0
Ryan Drost <rdrost@nerdery.com>
2014-09-27 07:23:20 -0500

    back to own custom admin panel
    too tough to try and get other admin panels to do the things that I
    want it to do.  Lets just go custom

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,13 @@
+module Admin
+  class BaseController < ApplicationController
+    before_filter :verify_admin
+
+  private
+
+    def verify_admin
+      redirect_to root_path, :alert => 'Unauthorized' unless current_user && current_user.is_admin?
+    end
+
+  end
+
+end
