e492da0b7112e6322ee242224ccf62469cf0031e
Ryan Drost <rdrost@nerdery.com>
2015-02-10 11:17:28 -0600

    remove coveralls and replace with Code Climate
    mine as well keep everything in one place and not use Code Climate and
    Coveralls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
-require 'coveralls'
-Coveralls.wear!('rails')
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -24,7 +24,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.use_transactional_fixtures = false
-  
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
@@ -49,7 +49,9 @@ RSpec.configure do |config|
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  # config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
+
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
