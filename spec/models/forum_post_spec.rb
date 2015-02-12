57c4ffb4f5854841616432cf01eea3f80992fb29
Ryan Drost <r.drost@nerdery.com>
2015-02-12 12:37:04 -0600

    couple tests for forum posts

--- spec/models/forum_post_spec.rb
+++ spec/models/forum_post_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe ForumPost do
   before(:each) do
-    @valid_attributes = { }
+    @valid_attributes = build(:forum_post).attributes
   end
 
   it "should create a new instance given valid attributes" do
-    # ForumPost.create!(@valid_attributes)
+    ForumPost.create!(@valid_attributes)
   end
+
+  it "will update the updated at date when saved" do
+    p = build(:forum_post, updated_at: 10.minutes.ago)
+    p.save
+    p.updated_at > 1.minutes.ago
+  end
+
 end
