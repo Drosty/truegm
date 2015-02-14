2890f108f114200aaef0ef3f1250d04da25c642e
Ryan Drost <rdrost@nerdery.com>
2015-02-14 12:24:40 -0600

    couple controller tests added

--- spec/controllers/nfl_players_controller_spec.rb
+++ spec/controllers/nfl_players_controller_spec.rb
@@ -2,10 +2,42 @@ require 'spec_helper'
 
 # Only actions to test are the Index and the Show
 describe NflPlayersController do
+  include Warden::Test::Helpers
+
+  before (:each) do
+    Warden.test_mode!
+  end
+
+  after (:each) do
+    Warden.test_reset!
+  end
+
   describe "GET index" do
-    it "assigns all nfl_players as @nfl_players" do
-      #Test things like pagination show up, players are there, sorting etc.
+    before(:each) do
+      @user = create(:user)
+      login(@user)
+
+      @league = create(:league, id: 1)
+      create_list(:nfl_player, 4, :qb)
+      create_list(:nfl_player, 6, :rb)
+      create_list(:nfl_player, 8, :wr)
+      create_list(:nfl_player, 7, :te)
+      create_list(:nfl_player, 5, :pk)
+      create_list(:nfl_player, 3, :def)
     end
+
+    it "it will return the number of players per page" do
+      request.accept = "application/json"
+      get :index, { :league_id => 1 }
+      assigns(:nfl_players).length.should == NflPlayer.per_page
+    end
+
+    it "it will return the correct league object" do
+      request.accept = "application/json"
+      get :index, { :league_id => 1 }
+      assigns(:current_league).should == @league
+    end
+
   end
 
   describe "GET show" do
