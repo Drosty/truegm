9e7fe33f5a1c92ff364551823323f5983b5b0c06
Ryan Drost <rdrost@nerdery.com>
2015-02-14 10:27:44 -0600

    league controller spec code moved to leagues controller spec

--- spec/controllers/leagues_controller_spec.rb
+++ spec/controllers/leagues_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe LeaguesController do
+  include Warden::Test::Helpers
 
-  #Delete this example and add some real ones
-  it "should use LeaguesController" do
-    controller.should be_an_instance_of(LeaguesController)
+  before (:each) do
+    Warden.test_mode!
+  end
+
+  after (:each) do
+    Warden.test_reset!
+  end
+
+  def valid_attributes
+    attributes_for(:league)
+  end
+
+  describe "User with no team" do
+    before(:each) do
+      user = create(:user)
+      login(user)
+    end
   end
 
 end
-    
