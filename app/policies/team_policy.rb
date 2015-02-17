d9a7c7c79f51ab8f803ec1fd0b6cfc9a1a481ca2
Ryan Drost <ryan.s.drost@gmail.com>
2015-02-16 21:04:37 -0600

    no need for policy scope

--- app/policies/team_policy.rb
+++ app/policies/team_policy.rb
@@ -3,9 +3,9 @@ class TeamPolicy < Struct.new(:user, :team)
   class Scope < Struct.new(:user, :scope)
     # Admins can access all posts on the index view,
     # any other user will only be able to see published ones
-    def resolve
-      scope
-    end
+    # def resolve
+    #  scope
+    # end
   end
 
   def update?
