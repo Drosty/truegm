f40a7f7f785d47c5f53de81e51753ff8b32b7a4f
Ryan Drost <ryan.s.drost@gmail.com>
2015-02-17 08:57:44 -0800

    use configuration in prod mode for the secret key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-EvokeTruegmRails::Application.config.secret_key_base = 'ba9179b6af71a37f0214ee1023d903c0df53862a58dbdbf9a362ce6b1f9d45d3e0744ebb9f13f1cdb724bed7290454108a54426ea847f393a0ef036729afcb9c'
+secret = Rails.env.production? ? ENV['SECRET_KEY_BASE'] : "NOTSECUREba9179b6af71a37f0214ee1023d903c0df53862a58dbdbf9a362ce6b1f9d45d3e0744ebb9f13f1cdb724bed7290454108a54426ea847f393a0ef036729afcb9c"
+EvokeTruegmRails::Application.config.secret_key_base = secret
