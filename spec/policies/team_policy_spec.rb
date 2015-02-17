7ea6a5d1f47fcbd11f5de21715d93d509e275373
Ryan Drost <ryan.s.drost@gmail.com>
2015-02-16 21:04:23 -0600

    team policy assertions added

--- spec/policies/team_policy_spec.rb
+++ spec/policies/team_policy_spec.rb
@@ -35,11 +35,36 @@ describe TeamPolicy do
     end
 
     it "can show teams that they are in the same league as" do
+      league = create(:league)
+      team1 = create(:team, league: league)
+      team2 = create(:team, league: league)
+      team3 = create(:team)
 
+      assert TeamPolicy.new(team1.user, team1).show?
+      assert TeamPolicy.new(team1.user, team2).show?
+      assert !TeamPolicy.new(team1.user, team3).show?
+
+      assert TeamPolicy.new(team2.user, team1).show?
+      assert TeamPolicy.new(team2.user, team2).show?
+      assert !TeamPolicy.new(team2.user, team3).show?
+
+      assert !TeamPolicy.new(team3.user, team1).show?
+      assert !TeamPolicy.new(team3.user, team2).show?
+      assert TeamPolicy.new(team3.user, team3).show?
     end
 
-    it "cannot show teams that are in leagues they are not part of" do
+    it "can update and edit team that user is attached too" do
+      team = build(:team)
+      team.user = user
+      assert TeamPolicy.new(user, team).update?
+      assert TeamPolicy.new(user, team).edit?
+    end
 
+    it "cannot update or edit a team user is not attached too" do
+      team = build(:team)
+      user = build(:user)
+      assert !TeamPolicy.new(user, team).update?
+      assert !TeamPolicy.new(user, team).edit?
     end
 
   end
