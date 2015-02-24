7f4f8531d06521ad81a3f7af9f0cee56b103651a
Ryan Drost <ryan.s.drost@gmail.com>
2015-02-24 07:55:22 -0600

    check the team name while we are there

--- spec/controllers/nfl_players_controller_spec.rb
+++ spec/controllers/nfl_players_controller_spec.rb
@@ -188,6 +188,7 @@ describe NflPlayersController do
       data = JSON.parse(response.body)
       data["is_on_team"].should == true
       data["is_free_agent"].should == false
+      data["league_team"]["name"].should == @league.teams.first.name
     end
 
   end
