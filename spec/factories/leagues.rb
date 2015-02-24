ff914e1c2bc2f7397f197e8e4553796ebdb3926d
Ryan Drost <ryan.s.drost@gmail.com>
2015-02-24 09:48:29 -0600

    update factory to include new league configuration values

--- spec/factories/leagues.rb
+++ spec/factories/leagues.rb
@@ -4,6 +4,25 @@ FactoryGirl.define do
     description "MyString"
     tagline "MyString"
 
+    player_salary_type "real"
+    min_team_players 14
+    max_team_players 15
+    starting_qb_slots 1
+    starting_rb_slots 2
+    starting_wr_slots 2
+    starting_te_slots 1
+    starting_d_slots 1
+    starting_flex_wrte_slots 0
+    starting_flex_rbwrte_slots 1
+    starting_k_slots 1
+    ir_slots 1
+    ir_players_tradeable false
+    ir_cap_relief_max 5000000
+    ir_re_activatable false
+    ir_reactivation_penalty 0
+    configuration_editable true
+    playoffs_start_week 13
+
     factory :league_with_stats do
       passing_yard_points 0.04
       passing_touchdown_points 5
