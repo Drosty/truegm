d3322124b7f5d146e70fd2e652fb17eb2411e597
Ryan Drost <ryan.s.drost@gmail.com>
2015-08-14 22:40:11 -0500

    add game_key to nfl_matchup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223222226) do
+ActiveRecord::Schema.define(version: 20150815033924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 20150223222226) do
     t.datetime "updated_at"
     t.float    "point_spread"
     t.float    "over_under"
+    t.string   "game_key"
   end
 
   create_table "nfl_players", force: :cascade do |t|
